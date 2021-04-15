21774ae1c34fe3925d45cf2ad6d133ea4fb19cf6
Kristijan Kostovski <kixer.wade2000@outlook.com>
2021-04-15 22:17:20 +0200

    Colliding with tiles.Movement and Camera Position

---
+++ ProjectPolutionGame/Assets/Movement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector2(-5, rb.velocity.y);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector2(5, rb.velocity.y);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(rb.velocity.x,5f);
+        }
+
+    }
+}
+
+
