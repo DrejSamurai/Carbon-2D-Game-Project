4ef8b6cd02638e3e2730c1eba7891e529ace584f
Kristijan Kostovski <kixer.wade2000@outlook.com>
2021-04-17 05:00:07 +0200

    Done with Movement with Sprite animations with animator

--- ProjectPolutionGame/Assets/PlayerMovement.cs
+++ ProjectPolutionGame/Assets/PlayerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
+    public Animator animator;
 
     public float runSpeed = 40f;
 
@@ -18,15 +19,22 @@ public class PlayerMovement : MonoBehaviour
 
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
         
         if (Input.GetButtonDown("Jump")) 
         {
             jump = true;
+            animator.SetBool("IsJumping", true);
         }
             
         
             
         
+    }
+
+    public void onLanding()
+    {
+        animator.SetBool("IsJumping", false);
     }
 
     void FixedUpdate()
