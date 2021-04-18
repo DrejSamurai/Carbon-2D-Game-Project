14c24e52424c36c41665e230b72a6c894b662ca5
Andrej Mishevski <andrej.mishevski@gmail.com>
2021-04-18 11:09:44 +0200

    Checkpoint System Working (sort of)

---
+++ ProjectPolutionGame/Assets/Checkpoints/PlayerPos.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PlayerPos : MonoBehaviour
+{
+    private GameMaster gm;
+    void Start()
+    {
+        gm = GameObject.FindObjectOfType<GameMaster>();
+        transform.position = gm.lastCheckPointPosition;
+    }
+
+   
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
